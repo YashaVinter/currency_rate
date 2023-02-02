de32066f01f7b402fbdb9bcb942eec40ec9b59b5
yasha <vinter-jasha@rambler.ru>
2023-02-02 21:09:59 +0500

    add actioncable rate channel

---
+++ app/javascript/channels/rate_channel.js
@@ -0,0 +1,17 @@
+import consumer from "./consumer"
+
+const RateChannel = consumer.subscriptions.create("RateChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+  }
+});
+
+export default RateChannel;
