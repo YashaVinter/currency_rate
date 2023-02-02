e1d5af88f46c45daa4c48786eb0325a31fb815a9
yasha <vinter-jasha@rambler.ru>
2023-02-02 21:11:16 +0500

    update routes, add actioncable, started api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => "/cable"
+  namespace :api do
+    namespace :v1 do
+      get 'rate/index'
+      post 'rate/create'
+    end
+  end
   root "homepage#index"
+  get '/*path' => 'homepage#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
