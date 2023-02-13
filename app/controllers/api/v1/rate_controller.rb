e8d90828943b31e32a4bbf61ffa08299609d1722
yasha <vinter-jasha@rambler.ru>
2023-02-13 21:33:00 +0500

    update RateController

--- app/controllers/api/v1/rate_controller.rb
+++ app/controllers/api/v1/rate_controller.rb
@@ -1,24 +1,35 @@
 class Api::V1::RateController < ApplicationController
   def index
-    url_1 = "https://api.coingate.com/v2/rates/merchant/USD/RUB"
-    url_2 = "https://www.cbr-xml-daily.ru/latest.js"
-    res =  RestClient.get(url_1).body
-    rate = { rate: res.to_f, time_stamp: Time.now.to_i }
+    if AdminRateService.set?
+      render json: AdminRateService.active_rate and return
+    end
+
+    rate = DollarRate.last.as_json(only: %i[rate timestamp])
     render json: rate
   end
 
   def create
-    p DollarRate.new(rate: rate_params[:new_rate], timestamp: rate_params[:new_rate_date])
-    ActionCable.server.broadcast 'RateChannel', {rate: rate_params[:new_rate]}.to_json
-
     admin_rate = AdminRate.create!(end_timestamp: rate_params[:new_rate_date], dollar_rate_attributes: { rate: rate_params[:new_rate], timestamp: Time.now.to_i })
+    AdminRateService.new(admin_rate).set_active
+    ActionCable.server.broadcast 'RateChannel', { rate: rate_params[:new_rate] }.to_json
+  rescue AdminRateService::NegativeTimestampError => e
+    render json: { error: e.message }, status: :unprocessable_entity
   end
 
-  def show
+  def admin_rates
+    last_rates_limit = 5
+    last_rates = AdminRate.includes(:dollar_rate).order(created_at: :desc).last(last_rates_limit)
+    last_rates_json = last_rates.as_json(
+      include: {
+        dollar_rate: {
+          only: %i[id rate timestamp]
+        }
+      },
+      only: :dollar_rate
+    )
+    render json: last_rates_json
   end
 
-  def destroy
-  end
   private
     def rate_params
       params.require(:rate).permit(:new_rate, :new_rate_date)
