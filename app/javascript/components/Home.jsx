f3e99d8f6d45d2de9edb03391f7e5380d5695fb8
yasha <vinter-jasha@rambler.ru>
2023-02-16 22:03:24 +0500

    update home page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import RateChannel from "../channels/rate_channel";
+import NavBar from "./NavBar";
 
 const Home = () => {
   const [rate, setRate] = useState();
 
   useEffect(() => {
-    RateChannel.connected = () => console.log("conn");
+    RateChannel.connected = () => console.log("connection established");
     RateChannel.received = (data) => {
       setRate(JSON.parse(data).rate)
     };
     const url = "/api/v1/rate/index";
-    // const token = document.querySelector('meta[name="csrf-token"]').content;
 
     fetch(url)
       .then((response) => {
@@ -29,19 +29,11 @@ const Home = () => {
   return (
     <div className="m-3">
       <div className="container">
-        <header className="d-flex justify-content-begin py-3">
-          <ul className="nav nav-pills">
-            <li className="nav-item"><Link to="/" className="nav-link active">Home</Link></li>
-            <li className="nav-item"><Link to="/admin" className="nav-link">Admin</Link></li>
-            <li className="nav-item"><a href="#" className="nav-link">About</a></li>
-          </ul>
-        </header>
+        <NavBar/>
         <h3 className=" primary-color" >Home page</h3>
         <hr/>
         <p>Dollar rate</p>
         <p>{ rate || "loading..."}</p>
-        <button className="btn btn-primary" onClick={()=>{setRate(rate+1)}}>Increment rate</button>
-        <hr />
       </div>
     </div>
   );
