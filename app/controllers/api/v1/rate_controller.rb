26180ce1e4f97500d6c825333b3d0cd2535c4202
yasha <vinter-jasha@rambler.ru>
2023-02-16 22:23:37 +0500

    add render json to RateController#create, change query and return json in admin_rates

--- app/controllers/api/v1/rate_controller.rb
+++ app/controllers/api/v1/rate_controller.rb
@@ -12,20 +12,21 @@ class Api::V1::RateController < ApplicationController
     admin_rate = AdminRate.create!(end_timestamp: rate_params[:new_rate_date], dollar_rate_attributes: { rate: rate_params[:new_rate], timestamp: Time.now.to_i })
     AdminRateService.new(admin_rate).set_active
     ActionCable.server.broadcast 'RateChannel', { rate: rate_params[:new_rate] }.to_json
+    render json: admin_rate
   rescue AdminRateService::NegativeTimestampError => e
     render json: { error: e.message }, status: :unprocessable_entity
   end
 
   def admin_rates
     last_rates_limit = 5
-    last_rates = AdminRate.includes(:dollar_rate).order(created_at: :desc).last(last_rates_limit)
+    last_rates = AdminRate.includes(:dollar_rate).order(created_at: :desc).first(last_rates_limit)
     last_rates_json = last_rates.as_json(
       include: {
         dollar_rate: {
           only: %i[id rate timestamp]
         }
       },
-      only: :dollar_rate
+      only: %i[dollar_rate end_timestamp]
     )
     render json: last_rates_json
   end
