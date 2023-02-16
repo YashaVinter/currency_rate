82bda562022fde372fb8f5a2f5f229918862af23
yasha <vinter-jasha@rambler.ru>
2023-02-16 22:06:08 +0500

    add About component

---
+++ app/javascript/components/About.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+
+const About = () => {
+  return (
+    <div className="m-3">
+      <div className="container">
+        <NavBar/>
+          About begin desc 
+      </div>
+    </div>
+  );
+};
+
+export default About;
